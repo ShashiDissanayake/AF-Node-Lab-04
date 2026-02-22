c1c8439faf30babb4ea2c7e649f5d733b004b511
ShashiDissanayake <sewminishashikala781@gmail.com>
2026-02-22 15:41:15 +0530

    Add write file

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 //console.log("Hello Node!");
 
+//Read file
 const fs = require('fs');
 fs.readFile('file.txt', 'utf8', function (err, data) {
  if (err) throw err;
  console.log(data);
+});
+
+//Write to file
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
 });
